a2d5c01a54101341ded4e922d831424c60cc225f
flow1981 <contact.f.stagliano@gmail.com>
2019-12-10 13:28:03 +0000

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,29 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+
+    constructor(props){
+        super(props)
+       // this.state = {key: "value"}
+      
+    }
+      
+    clickHandler = (event) => {
+        //console.log("clicked")
+        //console.log(event.screenX)
+        let coord = [event.screenX,event.screenY]
+        //console.log(coord)
+        this.props.onReceiveCoordinates(coord)
+    }
+
+    render(){
+        return (
+            <button
+               onClick={this.clickHandler}
+            >Coordinates
+            </button>
+        )
+    }
+
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,25 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component {
+
+    displayProps = () =>{
+        console.log(this.props)
+    }
+
+    handleClick = (event) => {
+        event.persist()
+        setTimeout(()=>{
+            this.props.onDelayedClick(event)},
+            this.props.delay)
+    }
+
+
+    render(){
+        this.displayProps()
+        return (
+            <button onClick={this.handleClick}>Delay</button>
+        )
+    }
+
+}
